b3d56b9be369ed9304ceecc1233a189b81403f30
Quinn Montgomery <quinninthetardis@gmail.com>
2019-03-14 21:56:24 -0400

    at some poit I will make the tile object do somthing.

--- tile.py
+++ tile.py
@@ -1,15 +1,7 @@
 
 
 class tile(object):
-<<<<<<< HEAD
     def __init__(self, type, name):
 
         self.type = type
         self.name = name
-=======
-    def __init__(self, type, name, size):
-
-        self.type = type
-        self.name = name
-        self.size = size
->>>>>>> d79af60296c382e853e617394d0377831f2c36be
